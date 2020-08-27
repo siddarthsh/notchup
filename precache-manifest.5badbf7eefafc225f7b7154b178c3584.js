c6c67ee7b81a85210d25f64768820530ec1cb6e8
Siddarth Sharma <siddarthsharma1077@gmail.com>
2020-08-27 21:57:30 +0530

    Updates

---
+++ precache-manifest.5badbf7eefafc225f7b7154b178c3584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e16e275b513afcda8dd187eacf43a7",
+    "url": "/notchup/index.html"
+  },
+  {
+    "revision": "4126af8c255724025e64",
+    "url": "/notchup/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "02069e02a8bb970f0026",
+    "url": "/notchup/static/css/main.858196cd.chunk.css"
+  },
+  {
+    "revision": "4126af8c255724025e64",
+    "url": "/notchup/static/js/2.3a9dc832.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/notchup/static/js/2.3a9dc832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02069e02a8bb970f0026",
+    "url": "/notchup/static/js/main.300dea3b.chunk.js"
+  },
+  {
+    "revision": "4d2d97d2b689429a30fe",
+    "url": "/notchup/static/js/runtime-main.7d982c59.js"
+  }
+]);
